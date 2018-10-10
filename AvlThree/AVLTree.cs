0d0ab55746c7cf83202555113fd8e3ebc904885c
Yurion987 <37672916+Yurion987@users.noreply.github.com>
2018-10-10 21:41:19 +0200

    Binary
    Basic rotations

---
+++ AvlThree/AVLTree.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvlThree
+{
+    class AVLTree<T> where T : IComparable<T>
+    {
+            public TNode<T> Root { get; set; }
+            public void Add(TNode<T> newone)
+            {
+                if (Root == null)
+                {
+                    Root = newone;
+                    return;
+                }
+                var tmpVrchol = Root;
+                while (true)
+                {
+                    //newone je mensi ako tmpVrchol
+                    if (newone.Value.CompareTo(tmpVrchol.Value) < 0)
+                    {
+                        if (tmpVrchol.Left == null)
+                        {
+                            newone.Parent = tmpVrchol;
+                            tmpVrchol.Left = newone;
+                            return;
+                        }
+                        tmpVrchol = tmpVrchol.Left;
+                    }
+                    // newone je vecsi ako tmpVrchol
+                    if (newone.Value.CompareTo(tmpVrchol.Value) > 0)
+                    {
+                        if (tmpVrchol.Right == null)
+                        {
+                            newone.Parent = tmpVrchol;
+                            tmpVrchol.Right = newone;
+                            return;
+                        }
+                        tmpVrchol = tmpVrchol.Right;
+                    }
+                    if (newone.Value.CompareTo(tmpVrchol.Value) == 0)
+                    {
+                        Console.WriteLine("Vrchol ma rovnake ID ako nachadzany");
+                        return;
+                    }
+                }
+            }
+
+            public void Deete(T todel)
+            {
+
+            }
+            public void PravaRotacia(TNode<T> rotovanyVrch)
+            {
+                //ma laveho syna 
+                if (rotovanyVrch.Left != null)
+                {
+                    var tmpVrchol = rotovanyVrch.Left;
+
+                    // syn ma praveho syna
+                    if (rotovanyVrch.Left.Right != null)
+                    {
+                        rotovanyVrch.Left = tmpVrchol.Right;
+                        tmpVrchol.Right = rotovanyVrch;
+                        rotovanyVrch.Left.Parent = rotovanyVrch;
+                    }
+                    //syn nema praveho syna
+                    else
+                    {
+                        rotovanyVrch.Left = null;
+                        tmpVrchol.Right = rotovanyVrch;
+
+                    }
+
+                    //rotovany vrchol ma parenta
+                    if (rotovanyVrch.Parent != null)
+                    {
+                        // som lavy syn parenta
+                        if (rotovanyVrch.Parent.Left == rotovanyVrch)
+                        {
+                            rotovanyVrch.Parent.Left = tmpVrchol;
+                        }
+                        else
+                        {
+                            rotovanyVrch.Parent.Right = tmpVrchol;
+                        }
+
+                        tmpVrchol.Parent = rotovanyVrch.Parent;
+                        rotovanyVrch.Parent = tmpVrchol;
+
+                    }
+                    else
+                    {
+                        tmpVrchol.Parent = null;
+                        Root = tmpVrchol;
+                        rotovanyVrch.Parent = tmpVrchol;
+                    }
+
+                }
+            }
+            public void LavaRotacia(TNode<T> rotovanyVrch)
+            {
+                //ma praveho syna 
+                if (rotovanyVrch.Right != null)
+                {
+                    var tmpVrchol = rotovanyVrch.Right;
+
+                    // syn ma laveho syna
+                    if (rotovanyVrch.Right.Left != null)
+                    {
+                        rotovanyVrch.Right = tmpVrchol.Left;
+                        tmpVrchol.Left = rotovanyVrch;
+                        rotovanyVrch.Right.Parent = rotovanyVrch;
+                    }
+                    //syn nema laveho syna
+                    else
+                    {
+                        rotovanyVrch.Right = null;
+                        tmpVrchol.Left = rotovanyVrch;
+
+                    }
+
+                    //rotovany vrchol ma parenta
+                    if (rotovanyVrch.Parent != null)
+                    {
+                        // som pravy syn parenta
+                        if (rotovanyVrch.Parent.Right == rotovanyVrch)
+                        {
+                            rotovanyVrch.Parent.Right = tmpVrchol;
+                        }
+                        else
+                        {
+                            rotovanyVrch.Parent.Left = tmpVrchol;
+                        }
+
+                        tmpVrchol.Parent = rotovanyVrch.Parent;
+                        rotovanyVrch.Parent = tmpVrchol;
+
+                    }
+                    else
+                    {
+                        tmpVrchol.Parent = null;
+                        Root = tmpVrchol;
+                        rotovanyVrch.Parent = tmpVrchol;
+                    }
+
+                }
+            }
+            public void InOrder(TNode<T> vrchol)
+            {
+                if (vrchol.Left != null)
+                {
+                    InOrder(vrchol.Left);
+                }
+
+                Console.Write(vrchol.Value);
+                if (vrchol.Right != null)
+                {
+                    InOrder(vrchol.Right);
+                }
+            }
+
+        }
+
+        public class TNode<T> where T : IComparable<T>
+        {
+
+            public T Value { get; set; }
+            public TNode<T> Left { get; set; }
+            public TNode<T> Right { get; set; }
+            public TNode<T> Parent { get; set; }
+            public int BalanceFactor { get; set; }
+
+
+        }
+
+
+    }
+
--- AvlThree/Program.cs
+++ AvlThree/Program.cs
@@ -10,6 +10,31 @@ namespace AvlThree
     {
         static void Main(string[] args)
         {
+            var a = new BTree<double>();
+            //    TNode<double> b = new TNode<double> { Value = 10 };
+            //    a.Add(b);
+            TNode<double> d = new TNode<double> { Value = 10 };
+            a.Add(d);
+            TNode<double> c = new TNode<double> { Value = 8 };
+            a.Add(c);
+            TNode<double> e = new TNode<double> { Value = 6 };
+            a.Add(e);
+            TNode<double> g = new TNode<double> { Value = 5 };
+            a.Add(g);
+            TNode<double> i = new TNode<double> { Value = 7 };
+            a.Add(i);
+            //       TNode<double> j = new TNode<double> { Value = 8.5 };
+            //     a.Add(j);
+            //       TNode<double> k = new TNode<double> { Value = 11 };
+            //      a.Add(k);
+            a.InOrder(a.Root);
+            Console.WriteLine();
+            a.PravaRotacia(c);
+            a.InOrder(a.Root);
+            Console.WriteLine();
+            a.LavaRotacia(e);
+            a.InOrder(a.Root);
         }
+    }
     }
 }
